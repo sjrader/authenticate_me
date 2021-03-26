36a9a222442f153d35be0b3cf3d0104a203efee7
Sean Rader <seanrader@Seans-MacBook-Pro.local>
2021-03-26 13:58:02 -0700

    signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
+import SignupFormPage from './components/SignupFormPage';
 import * as sessionsActions from './store/session';
 
 function App() {
@@ -16,8 +17,8 @@ function App() {
       <Route path="/login">
         <LoginFormPage /> 
       </Route>
-      <Route path="/">
-        <h2>Test</h2>
+      <Route path="/signup">
+        <SignupFormPage />
       </Route>
     </Switch>
   );
