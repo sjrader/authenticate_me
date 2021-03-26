1f4e753979048bce37c83fc55514a51402bcb716
Sean Rader <seanrader@Seans-MacBook-Pro.local>
2021-03-26 13:28:47 -0700

    Restore User

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
+import * as sessionsActions from './store/session';
 
 function App() {
-  return (
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionsActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch])
+
+  return isLoaded && (
     <Switch>
       <Route path="/login">
         <LoginFormPage /> 
+      </Route>
+      <Route path="/">
+        <h2>Test</h2>
       </Route>
     </Switch>
   );
